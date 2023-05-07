d928b348a240a5616ae55b40c665612f8368b47c
Cole Kauder-McMurrich <gamingwithcole7@gmail.com>
2023-05-07 18:55:21 -0400

    Add onekey

--- keyboards/onekey/onekey.rs
+++ keyboards/onekey/onekey.rs
@@ -8,13 +8,13 @@ use rp_pico::hal::gpio::DynPin;
 
 #[entry]
 fn main() -> ! {
-    const NUMOFCOL: usize = 2;
+    const NUMOFCOL: usize = 1;
     const NUMOFROW: usize = 1;
-    const KEYS: [[u8; NUMOFCOL]; NUMOFROW] = [[0x04, 0x06]];
+    const KEYS: [[u8; NUMOFCOL]; NUMOFROW] = [[0x05]];
 
     let (pins, watchdog) = init();
 
-    let col: [DynPin; NUMOFCOL] = [pins.gpio28.into(), pins.gpio17.into()];
+    let col: [DynPin; NUMOFCOL] = [pins.gpio17.into()];
     let row: [DynPin; NUMOFROW] = [pins.gpio16.into()];
 
     matrix_scaning(col, row, KEYS, watchdog);
